dcb58fd315e309e288f26c5aebab0d2d5e9b58cc
Yogitha251 <74713251+Yogitha251@users.noreply.github.com>
2022-05-10 12:33:38 +0530

    Add files via upload

---
+++ automation.py
@@ -0,0 +1,13 @@
+start=input('enter the unit you want to convert to cm choose from : 1. meters , 2. kilometeres')
+ans=int(input('enter the value:'+start+'='))
+
+meters= ans*100
+kilometeres= ans*1000
+
+print(meters)
+print(kilometeres)
+
+
+
+
+
